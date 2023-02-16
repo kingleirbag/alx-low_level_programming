b29fce0f8dbf48c20ca66fe9593b1a31cfb2d55a
Gabriel Muthenya <gmthex@gmail.com>
2023-02-16 06:35:22 -0800

    Adding comments to the code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * Main entry point
+ * print using puts
+ * When success return 0
+ **/
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
