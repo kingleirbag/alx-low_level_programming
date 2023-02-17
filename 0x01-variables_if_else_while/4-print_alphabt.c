a3c311316562c11cc146e9fde9532bc13bd8b947
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 05:29:03 -0800

    Using if function

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdion.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+		if (character != 'q' || character != 'e')
+			putchar(character);
+
+	putchar('\n');
+	return (0);
+}
