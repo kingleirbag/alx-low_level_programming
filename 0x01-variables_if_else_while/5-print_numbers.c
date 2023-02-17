7d0955d259fa8dd8d49cb5be230d937f2b872b08
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 05:42:04 -0800

    Print digit form 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - Print all digits to base 10
+ *
+ * Return - Always 0 (Success)
+ */
+int main (void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 10; digit++)
+		printf("%d\n", digit);
+
+	returnt (0)
+}
