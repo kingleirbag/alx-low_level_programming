44459d4b58a3be64b4196bc6169c38d120bcac5f
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 04:28:20 -0800

    Removing trailing whitespace

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
- * main - Entry point
+/**
+ * main - Entry point for the function
  *
  * Description - Prints the last digit of a randomly generated number
  * and whether it is greater than 5, less than 6, or 0.
