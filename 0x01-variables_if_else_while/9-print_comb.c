b6c42b193809f072050c8bc03a45906c34359f7d
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 06:49:17 -0800

    Adding curly brackets

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,13 @@ int main(void)
 	int digit;
 
 	for (digit = 0; digit <= 9; digit++)
+	{
 		putchar('0' + digit);
 		if (digit < 0)
 			putchar(',');
 			putchar(' ');
-	
+	}
+
 	putchar('\n');
 
 	return (0);
