f2c8b51498092e9baf1bdacb7bede72fa9a598b3
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 06:14:25 -0800

    Print charatecter in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print lowercase in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char character;
+
+	for (character = 'z'; character => 'a'; character--)
+		putchar(character);
+
+	putchar('\n');
+
+	returnt (0);
+}
