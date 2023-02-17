c846d0817cb823194c2a6fa9852016a7ad51a85f
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 06:39:06 -0800

    Ascending separated by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Print all single digit in assending separated by ', '
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+		putchar('0' + digit);
+		putchar(', ');
+	
+	putchar('\n');
+
+	return (0);
+}
