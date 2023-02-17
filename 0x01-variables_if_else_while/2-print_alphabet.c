d654944aaaa52a74f06b887931bbaa8448188788
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 04:58:26 -0800

    Prict character using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poin
+ *
+ * Desccription: Print character using puctchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+		putchar(c);
+		putchar('\n');
+
+	return (0);
+}
