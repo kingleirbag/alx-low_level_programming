faf4fb2927a8b43e0fe4939ce5eae797f86ba674
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 04:21:47 -0800

    Removing white space

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+i#include <stdio.h>
 
 /*
  * main - Entry point
+ *
+ * Description - Prints the last digit of a randomly generated number
+ * 		 and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0 (Succees)
  */
 int main(void)
 {
 	int n;
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if ((n % 10) == 0)
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	else if ((n % 10) > 5)
