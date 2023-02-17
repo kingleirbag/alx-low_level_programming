1adca966c3b51e38a779fd49436b035d14497bcb
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 04:25:56 -0800

    Removing trailing whitespace

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,14 +6,14 @@
  * main - Entry point
  *
  * Description - Prints the last digit of a randomly generated number
- * 		 and whether it is greater than 5, less than 6, or 0.
+ * and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0 (Succees)
  */
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) == 0)
