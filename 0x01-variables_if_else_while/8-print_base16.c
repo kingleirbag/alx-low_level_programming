76a9d9896b5f53034891a770875b2995b4b58e88
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 06:27:22 -0800

    Print base 16 characters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print base 16 values
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+	char character;
+
+	for (digit = 0; digit <=9; digit++)
+		putchar('0' + digit);
+
+	for (character = 'a'; character <= 'f'; character++)
+		putchat(character);
+
+	putchar('\n');
+
+	return (0);
+}
