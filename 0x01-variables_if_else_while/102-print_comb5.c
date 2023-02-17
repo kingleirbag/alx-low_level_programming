82b5cb37c7e71c5a030df8b4f169b22142f03cea
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 07:18:02 -0800

    Combining first letter and last letter

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Print all number from 0 to 99
+ *
+ * Return: Always 0 (Success)
+ * */
+int main(void)
+{
+	int firstDigits;
+	int seccondDigits;
+
+	for (firstDigits = 0; firstDigits <= 98; firstDigits++)
+	{
+		for (seccondDigits = firstDigits + 1; seccondDigits <= 99; seccondDigits++)
+		{
+			putchar((firstDigits / 10) + '0');
+			putchar((firstDigits % 10) + '0');
+			putchar(' ');
+			putchar((seccondDigits / 10) + '0');
+			putchar((seccondDigits % 10) + '0');
+
+			if (firstDigits == 98 && seccondDigits == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
