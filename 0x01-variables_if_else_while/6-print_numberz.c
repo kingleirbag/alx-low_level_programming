e2b6b807533591111ccbf174e71d9349f688ca88
Gabriel Muthenya <gmthex@gmail.com>
2023-02-17 06:01:15 -0800

    Print int using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description -  Print int using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 0; digit++)
+		putchar('0' + digit);
+
+	putchar("\n");
+
+	return (0);
+}
