134516f4768c1eb029bef29d9ba4578334256cd1
Gabriel Muthenya <gmthex@gmail.com>
2023-02-21 07:04:16 -0800

    Print_putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main - Print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char character[] = "_putchar";
+
+	for(int i = 0; i < 8; i++)
+	{
+		_putchar(character[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
