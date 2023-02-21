90cfbea91898395725cab0ad9cf484bf5d961357
Gabriel Muthenya <gmthex@gmail.com>
2023-02-21 07:35:33 -0800

    Print all lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print the lowercase alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
