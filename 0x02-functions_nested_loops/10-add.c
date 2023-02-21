861f52893370c483e5a47fe68326ebdf7c35b682
Gabriel Muthenya <gmthex@gmail.com>
2023-02-21 11:01:20 -0800

    Addition of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: Add two numbers
+ *
+ * @x: int type number
+ * @y: int type number
+ *
+ * Return: result of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
