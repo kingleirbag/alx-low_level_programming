52711e1a7fd8b786e075addeb817fdd69458a8cc
Gabriel Muthenya <gmthex@gmail.com>
2023-02-21 08:52:22 -0800

    Check for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * @c: the integer value
+ *
+ * Descripion: Checks if a character is lower case or upper case
+ *
+ * Return: 1 if true else 0 if false
+ *
+ */
+int _isalpha(int c)
+{
+        int i;
+
+        for (i = 'a'; i <= 'z'; i++)
+        {
+                /* Check for int c*/
+                if (c == i)
+                {
+                        return (1);
+                }
+        }
+	for (i = 'A'; i <= 'Z'; i++)
+        {
+                /* Check for int c*/
+                if (c == i)
+                {
+                        return (1);
+                }
+        }
+        return (0);
+}
