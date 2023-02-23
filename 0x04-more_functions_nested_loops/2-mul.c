77dd397cf2f96cac60a3f006b251d35855c4b737
Gabriel Muthenya <gmthex@gmail.com>
2023-02-23 06:52:08 -0800

    get product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - get the product of a and b
+ * @a: first variable
+ * @b: second variable
+ * 
+ * Returns: product of a and b (a * b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
