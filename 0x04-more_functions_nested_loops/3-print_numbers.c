69ea05b3b443c203f4198e95fd32ae764c8de553
Gabriel Muthenya <gmthex@gmail.com>
2023-02-23 08:40:19 -0800

    Updated 3-print_numbers.c file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,10 @@
  */
 void print_numbers(void)
 {
-	char c;
-	
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-		_putchar('\n');
-	}
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
 }
