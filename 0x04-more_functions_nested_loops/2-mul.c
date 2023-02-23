f6e5763147b150e6e34b5071f04eee661a40c80b
Gabriel Muthenya <gmthex@gmail.com>
2023-02-23 08:20:21 -0800

    Fix product return

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * mul - get the product of a and b
  * @a: first variable
  * @b: second variable
  * 
- * Return: product of a and b
+ * Return: product
  */
 int mul(int a, int b)
 {
-	return (a * b);
+	int x;
+
+	x = a * b;
+	return (x);
 }
