a65e005243c362e06134c051bdecf1133b4a8d76
Gabriel Muthenya <gmthex@gmail.com>
2023-02-23 07:50:10 -0800

    Print loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - prints # using _putchar function
+ * @n: the integer to print
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	x = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
+}
