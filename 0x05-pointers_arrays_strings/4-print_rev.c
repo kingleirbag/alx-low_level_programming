dfe7a9084850094b5d58749b3dd0b17ebeaa489d
Gabriel Muthenya <gmthex@gmail.com>
2023-02-27 02:52:42 -0800

    Print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ void print_rev(char *s)
 	for (length = 0; s[length] != '\0'; length++)
 	{
 	}
-	for (length = length -1; length >= 0; length--)
+	for (length = length - 1; length >= 0; length--)
 	{
 		_putchar(s[length]);
 	}
