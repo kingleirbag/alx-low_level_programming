beee79e3e61da029658138a07a3a10d1c20ee16a
Gabriel Muthenya <gmthex@gmail.com>
2023-02-27 02:28:53 -0800

    prints a string and a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: argument to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int length = 0;
+
+	while (str[length])
+	{
+		_putchar(str[length]);
+		length++;
+	}
+	_putchar('\n')
+}
