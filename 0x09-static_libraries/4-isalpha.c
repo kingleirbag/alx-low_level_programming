30ac5c039df4067849a97bc2cd9caeaa3dd21f7e
Gabriel Muthenya <gabriel@bildit.co>
2023-03-11 00:30:17 +0300

    Added files

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * @c: the integer value
+ *
+ * Descripion: Checks if a character is lower case or upper case
+ *
+ * Return: 1 if true else 0 if false
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
