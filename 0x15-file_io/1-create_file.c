9b816f3f921e67769a4ee7265853a39ab9cc83e8
Gabriel Muthenya <gmthex@gmail.com>
2023-04-10 13:28:17 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * create_file - a function that creates a file.
