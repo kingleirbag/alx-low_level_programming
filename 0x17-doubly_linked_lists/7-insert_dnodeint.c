027c581a0e4161f29c3a93839295b613ce807422
Gabriel Muthenya <gmthex@gmail.com>
2023-05-18 11:01:03 -0700

    Ne node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (node->next == NULL)
 		return (add_dnodeint_end(h, n));
 	new_node = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = node;
